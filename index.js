3b573c575b9c813495e326b99bb54126ee259f80
Cesium <cesium@cesium.one>
2025-10-22 03:48:41 -0400

    we dont need two pings for one cock

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const token = process.env.DISCORD_TOKEN
 
 // the basic discord setup stuff yoinked from their guide
-const { Client, Events, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Partials, ActivityType, MessageFlags } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, 
@@ -33,7 +33,7 @@ client.on(Events.MessageCreate, message => {
         console.log(`Chat: Detected ${original}, girlcocking it!`);
         const cocklink = `https://girlcockx.com/${profile}/status/${stub}`
         console.log(`Girlcock: Converted to ${cocklink}`)
-        message.channel.send(cocklink)
+        message.channel.send({ content: cocklink, flags: MessageFlags.SuppressNotifications })
         message.suppressEmbeds().catch(err =>
             // this next bit just cuts down the error to the important part, which will usually end up being "no permissions"
             console.error(err.stack?.split('\n')[0] || err.message || String(err).split('\n')[0])
