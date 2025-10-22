480de70b39b804b2302f22dbbe696874617ae147
Cesium <cesium@cesium.one>
2025-10-21 22:40:42 -0400

    functioning start

---
+++ README.md
@@ -0,0 +1,10 @@
+# girl cock inator
+
+behold perry the platypus, the fine ill do it for you inator
+
+## usage
+
+hopefully `npm i` should just get the dependencies
+
+and copy default.config.json to config.json and edit in at least the discord
+token i dont think i even reference the client id in there lol
---
+++ index.js
@@ -0,0 +1,37 @@
+const config = require('./config.json');
+
+// the basic discord setup stuff yoinked from their guide
+const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages, 
+        GatewayIntentBits.MessageContent
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message
+      ]
+});
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Discord: Connected as ${readyClient.user.tag}`);
+});
+client.login(config.token);
+
+client.on(Events.MessageCreate, message => {
+    // if we smell a twitter link, girlcock it!
+    const regexProfile = /https?:\/\/x\.com\/(.*?)\/status\/(\d+)/;
+    if (message.content.match(regexProfile)) {
+        const original = message.content.match(regexProfile)[0]
+        const profile = message.content.match(regexProfile)[1]
+        const stub = message.content.match(regexProfile)[2]
+        console.log(`Chat: Detected ${original}, girlcocking it!`);
+        const cocklink = `https://girlcockx.com/${profile}/status/${stub}`
+        console.log(`Girlcock: Converted to ${cocklink}`)
+        message.channel.send(cocklink)
+        message.suppressEmbeds().catch(err => // this bit is ai generated because i am lazy
+            console.error(err.stack?.split('\n')[0] || err.message || String(err).split('\n')[0])
+        )
+    }
+});
