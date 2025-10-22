cc3e9a66b69202a3d1edc51c5aebb32a34680aa6
Cesium <cesium@cesium.one>
2025-10-21 23:56:13 -0400

    it didnt like that idc

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const client = new Client({
 });
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Discord: Connected as ${readyClient.user.tag}`);
-    client.user.setActivity('for x.com links', { type: ActivityType.Watching });
 });
 client.login(token);
 
