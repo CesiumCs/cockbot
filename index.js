8902e46f25d733c0da9881684e415b25f1c3d646
Cesium <cesium@cesium.one>
2025-10-21 23:57:37 -0400

    i fixed it i hope, i just wanted it to be online still and my workflow currently is test on prod and pray

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const token = process.env.DISCORD_TOKEN
 
 // the basic discord setup stuff yoinked from their guide
-const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, 
@@ -16,6 +16,7 @@ const client = new Client({
 });
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Discord: Connected as ${readyClient.user.tag}`);
+    client.user.setActivity('for x.com links', { type: ActivityType.Watching });
 });
 client.login(token);
 
